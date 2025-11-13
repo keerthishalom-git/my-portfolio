ec1ba78d160f677b322c5e3ef4144e1df4185659
Keerthi Shalom Vaddepalli <keerthimadhurivaddepalli@gmail.com>
2025-11-12 23:38:48 -0600

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+```
+
+## 8. `.gitignore`
+```
+# Dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# Testing
+/coverage
+
+# Production
+/build
+
+# Misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+# IDE
+.vscode/
+.idea/
+*.swp
+*.swo
+*~
